0a3bb059e6c5f18331543d03214a56a2bf4a6f5f
Damien Richcreek <damien@richcreek.dev>
2022-06-15 15:43:12 -0700

    created ability to fill out form and submit new record

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -9,5 +9,16 @@ class BlogPostController < ApplicationController
     def new
         @blog_posts = BlogPost.new
     end
+    def create
+        @blog_post = BlogPost.create(blog_post_params)
+        if @blog_post.valid?
+            redirect_to blog_posts_path
+        end
+    end
+
+    private
+    def blog_post_params
+        params.require(:blog_post).permit(:title, :content)
+    end
 
 end
