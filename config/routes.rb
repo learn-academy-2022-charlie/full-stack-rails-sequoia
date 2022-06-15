d89f637d7842c3d4db3710d7ebf230d9342b7310
Nic Last <niclast7611@gmail.com>
2022-06-15 11:26:28 -0700

    completed show and can navigate to indiviual posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
  get 'blog_posts' => 'blog_post#index', as: 'blog_posts'
+ get 'blog_posts/:id' => 'blog_post#show', as: 'blog_post'
  root 'blog_post#index'
 end
+
+
